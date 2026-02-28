349f10e392efb18a4c38352b9148689e4cb0e2bb
noura <noura.fahad2022@gmail.com>
2026-02-28 19:54:33 +0000

    fix

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,6 +1,32 @@
 #include "main.h"
 
 /**
+ * print_number - prints a number with correct spacing
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	if (n < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n < 100)
+	{
+		_putchar(' ');
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+}
+
+/**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of times table
  */
@@ -18,32 +44,12 @@ void print_times_table(int n)
 			result = i * j;
 
 			if (j == 0)
-			{
 				_putchar('0');
-			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				if (result < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				else if (result < 100)
-				{
-					_putchar(' ');
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				else
-				{
-					_putchar((result / 100) + '0');
-					_putchar(((result / 10) % 10) + '0');
-					_putchar((result % 10) + '0');
-				}
+				print_number(result);
 			}
 		}
 		_putchar('\n');
