766387fbd1dbc79577120f5d87073a7ef19164bf
noura <noura.fahad2022@gmail.com>
2026-02-28 20:02:12 +0000

    add

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even Fibonacci numbers not exceeding 4000000
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int a = 1, b = 2, next;
+	unsigned long int sum = 0;
+
+	while (b <= 4000000)
+	{
+		if (b % 2 == 0)
+			sum += b;
+
+		next = a + b;
+		a = b;
+		b = next;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
