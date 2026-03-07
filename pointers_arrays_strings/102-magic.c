0428af1283488275e7dcc6eb6891704a817fe587
noura <noura.fahad2022@gmail.com>
2026-03-07 17:46:10 +0000

    task 10

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,19 +1,22 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-p[5] = 98
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int *p;
+	char a[5];
+	char *p2;
+	p = (int *)a;
+	p2 = (char *)p;
+	p2[0] = 98;
+	p2[1] = 0;
+	p2[2] = 0;
+	p2[3] = 0;
+	p2[4] = 0;
+	p[5] = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
