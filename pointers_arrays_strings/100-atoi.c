f4850d5e0437372878325d27d0dde18d74e64a83
noura <noura.fahad2022@gmail.com>
2026-03-07 16:30:01 +0000

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,19 +10,19 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	int num = 0;
-	int started = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == '-' && started == 0)
+		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			started = 1;
 			num = num * 10 + (s[i] - '0');
+
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+				break;
 		}
-		else if (started == 1)
-			break;
 		i++;
 	}
 
