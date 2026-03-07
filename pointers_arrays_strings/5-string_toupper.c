8c2ed148ed8200919b887859d8ed8acac44eb86f
noura <noura.fahad2022@gmail.com>
2026-03-07 16:57:29 +0000

    task 6

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: string
+ * Return: pointer to the string
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+
+	return (s);
+}
