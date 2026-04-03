2dcb4ea5c76e817be6d93192c923536e259d3f2d
noura <noura.fahad2022@gmail.com>
2026-04-03 11:36:55 +0000

    task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -6,7 +6,7 @@
  * @d: pointer to struct dog
  * @name: name of the dog
  * @age: age of the dog
- * @owner: pointer to owner name
+ * @owner: owner of the dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,13 @@
  * @age: age of the dog
  * @owner: owner of the dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
