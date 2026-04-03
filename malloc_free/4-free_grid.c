30f694218873e45afd1214f45d9e65332d5c5e36
noura <noura.fahad2022@gmail.com>
2026-04-03 10:58:59 +0000

    task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2D grid
+ * @grid: pointer to grid
+ * @height: number of rows
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
