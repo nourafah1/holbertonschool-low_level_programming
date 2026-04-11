fb8966cdea5c85935efc62458e205809b4a6f24c
noura <noura.fahad2022@gmail.com>
2026-04-11 17:42:33 +0000

    task 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - calculator
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: result
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", func(a, b));
+
+	return (0);
+}
