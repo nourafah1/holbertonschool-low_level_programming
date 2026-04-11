d63bdc673e013d27b61925edd2fb6a605f4b062e
noura <noura.fahad2022@gmail.com>
2026-04-11 17:12:39 +0000

    task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,6 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
-/**
- * print_name - prints a name using a function
- * @name: the name
- * @f: pointer to function
- */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
