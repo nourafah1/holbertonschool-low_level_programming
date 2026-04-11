a50918b56f1cf085e4ecf1b99a32aed8d0df8e4f
noura <noura.fahad2022@gmail.com>
2026-04-11 18:02:06 +0000

    task adv

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+	unsigned char *opcodes = (unsigned char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%.2x", opcodes[i]);
+		if (i != bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
