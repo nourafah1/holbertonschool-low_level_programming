fd90f047c0692826b93e984c3fdf3d62fbbfed12
noura <noura.fahad20222@gmail.com>
2026-04-27 22:29:27 +0000

    create hash table

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,33 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: key
+ * @value: value
+ * @next: pointer to next node
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table
+ * @size: size of array
+ * @array: array of linked lists
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+/* prototype */
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
